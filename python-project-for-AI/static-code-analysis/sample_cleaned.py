adae78769c78fe62ef4a9c750d0e6b9b0a52794a
Topgyal Gurung <topgyaltsering3@gmail.com>
2023-01-16 18:35:54 -0500

    note on static code analysis

---
+++ python-project-for-AI/static-code-analysis/sample_cleaned.py
@@ -0,0 +1,10 @@
+''' add two numbers '''
+def add(number1,number2):
+    """ add two number """
+    return number1+number2
+
+NUM1 = 4
+NUM2 = 5
+TOTAL = add(NUM1, NUM2)
+
+print(f"The sum of {NUM1} and {NUM2} is {TOTAL}")
